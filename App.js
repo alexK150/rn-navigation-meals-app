8c832624479450016b655b620dad77eff4b4942c
alex kuzin <akuzin20@gmail.com>
2021-05-25 12:21:35 +0300

    added expo dependencies for navigation, fonts and file for navigation set up

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo/build/removed.web';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [isFontLoaded, setFontLoaded] = useState(false);
+
+  if (!isFontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+    />
+  }
+
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
